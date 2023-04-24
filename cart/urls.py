47d5dbb0c6a756ca7f3519915da39941177b46b3
imbharat420 <imbharat420@gmail.com>
2023-04-24 15:21:04 +0530

    registeration and login

--- cart/urls.py
+++ cart/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('cart/', views.cart, name='cart'), 
     path('cart/add_cart/<int:product_id>', views.add_cart, name='add_cart'), 
-    path('cart/remove_cart/<int:product_id>', views.remove_cart, name='remove_cart'), 
-    path('cart/remove_cart_item/<int:product_id>', views.remove_cart_item, name='remove_cart_item'), 
+    path('cart/remove_cart/<int:product_id>/<int:cart_item_id>', views.remove_cart, name='remove_cart'), 
+    path('cart/remove_cart_item/<int:product_id>/<int:cart_item_id>', views.remove_cart_item, name='remove_cart_item'), 
     path('wishlist/', views.wishlist, name='wishlist'), 
 ]
--- user/views.py
+++ user/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
 from django.shortcuts import redirect
+from .forms import RegistrationForm
+from django.contrib.auth.decorators import login_required
 
 def login(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
+
+        print(username,password)
+
         user = auth.authenticate(username=username, password=password)
         if user is not None:
             auth.login(request, user)
@@ -20,30 +25,59 @@ def login(request):
 
 def signup(request):
     if(request.method == 'POST'):
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        cpassword = request.POST['con-password']
-       
+        # username = request.POST['username']
+        # email = request.POST['email']
+        # phone = request.POST['phone']
+        # password = request.POST['password']
+        # cpassword = request.POST['confirm_password']
 
-        if password == cpassword:
-            if User.objects.filter(username=username).exists():
-                messages.error(request, 'That username is taken')
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            try:
+                username = form.cleaned_data['username']
+                email = form.cleaned_data['email']
+                phone = form.cleaned_data['phone']
+                password = form.cleaned_data['password']
+
+                print(username,email,phone,password)
+
+                user = User.objects.create_user(username=username, password=password,email=email)
+                user.phone = phone
+                
+                print(user)
+                user.save()
+                messages.success(request, 'You are now registered and can log in')
+                return redirect('login')
+            except:
+                messages.error(request, 'Already exists username or email')
                 return redirect('signup')
-            else:
-                if User.objects.filter(email=email).exists():
-                    messages.error(request, 'That email is being used')
-                    return redirect('signup')
-                else:
-                    user = User.objects.create_user(username=username, password=password,email=email)
-                    user.save()
-                    messages.success(request, 'You are now registered and can log in')
-                    return redirect('login')
         else:
             messages.error(request, 'Passwords do not match')
             return redirect('signup')
     else:
-        return render(request, 'user/register.html')
+      form = RegistrationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'user/register.html',context)
+    # if password == cpassword:
+    #     if User.objects.filter(username=username).exists():
+    #         messages.error(request, 'That username is taken')
+    #         return redirect('signup')
+    #     else:
+    #         if User.objects.filter(email=email).exists():
+    #             messages.error(request, 'That email is being used')
+    #             return redirect('signup')
+    #         else:
+    #             user = User.objects.create_user(username=username, password=password,email=email)
+    #             user.save()
+    #             messages.success(request, 'You are now registered and can log in')
+    #             return redirect('login')
+    # else:
+    #     messages.error(request, 'Passwords do not match')
+    #     return redirect('signup')
+   
 
 def settings(request):
     if(request.method == 'POST'):
@@ -57,8 +91,11 @@ def settings(request):
     return render(request, 'user/settings.html')
 
 
+
+
+
+@login_required(login_url = 'login')
 def logout(request):
-  if request.method == 'POST':
     auth.logout(request)
-    messages.success(request, 'You are now logged out')
-    return redirect('home')
+    messages.success(request, 'You are logged out.')
+    return redirect('login')
